1b3194199e111cc2031a814f8c6442b8a0323626
Moisés <moisesvidal09@gmail.com>
2019-10-25 17:07:07 -0300

    Code indentation

--- app/src/main/java/com/example/localizacao/MainActivity.java
+++ app/src/main/java/com/example/localizacao/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.localizacao;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,100 +17,113 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Objects;
-
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private LatLng currentPosition;
 
     private GoogleMap map;
 
     private LocationManager locationManager;
 
-    private LatLng lastLocation;
+    private LatLng localizacaoAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-
+        //Aqui verificamos a permissão para utilizar o gps
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
+            //Aqui verificamos a permissão para utilizar o gps
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
+                // Neste momento iniciamos o mapa chamando o metodo onMapReady
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+
             } else {
+                //Caso não tivermos permissão, iremos perguntar para o usuário se ele deseja permitir
+                mostrarMensagem("É necessário permitir o uso de sua localização");
+
                 ActivityCompat.requestPermissions(this, new String[]{
                         Manifest.permission.ACCESS_FINE_LOCATION
                 }, 0);
 
             }
         } else {
+            //Caso não tivermos permissão, iremos perguntar para o usuário se ele deseja permitir
+            mostrarMensagem("É necessário permitir o uso de sua localização");
+
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_COARSE_LOCATION
             }, 0);
-
         }
 
     }
 
+    /**
+     * Método responsável por preparar o mapa
+     * @param googleMap
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Criteria criteria = new Criteria();
-
-        String provider = locationManager.getBestProvider(criteria, true);
-
         map = googleMap;
 
-//        map.setOnMapClickListener();
-
+        // Aqui habilitamos os controles de ZOOM no mapa
         map.getUiSettings().setZoomControlsEnabled(true);
 
+        // Aqui habilitamos para visualizarmos a posição atual, se não o mapa não terá o ponto azul, ou seja, nossa posição atual
         map.setMyLocationEnabled(true);
 
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
+        //Verificamos as permissões do GPS novamente
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // Aqui recebemos a ultima localizacao obtida do usuário pela INTERNET
+            Location localizacaoViaInternet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            // Aqui recebemos a ultima localizacao obtida do usuário pelo GPS
+            Location localizacaoViaGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            
+            //Verificamos se foi obtido a localização via INTERNET
+            if (localizacaoViaInternet != null) {
+
+                //Obtendo latitude e longitude
+                double latitude = localizacaoViaInternet.getLatitude();
+                double longitude = localizacaoViaInternet.getLongitude();
+                localizacaoAtual = new LatLng(latitude, longitude);
+
+            }
+            //Verificamos se foi obtido a localização via GPS
+            else if (localizacaoViaGPS != null) {
+
+                //Obtendo latitude e longitude
+                double latitude = localizacaoViaGPS.getLatitude();
+                double longitude = localizacaoViaGPS.getLongitude();
+                localizacaoAtual = new LatLng(latitude, longitude);
+
+            }
+
+
+            //Aqui marcamos o local do usuário com um Ponteiro
+            map.addMarker(new MarkerOptions().position(localizacaoAtual).title("Sua Posicação!"));
+
+            //Aqui movimentamos a tela para a posição atual do usuário com uma animação de ZOOM
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(localizacaoAtual, 17.5f));
+
         }
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
 
-        Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-        Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-        if (location != null) {
-            double latti = location.getLatitude();
-            double longi = location.getLongitude();
-            lastLocation = new LatLng(latti,longi);
-
-        } else if (location1 != null) {
-            double latti = location1.getLatitude();
-            double longi = location1.getLongitude();
-            lastLocation = new LatLng(latti,longi);
-        } else if (location2 != null) {
-            double latti = location2.getLatitude();
-            double longi = location2.getLongitude();
-            lastLocation = new LatLng(latti,longi);
-        }
-
-
-        map.addMarker(new MarkerOptions().position(lastLocation).title("Sua Posicação!"));
-
-        map.moveCamera(CameraUpdateFactory.newLatLng(lastLocation));
-
+    /**
+     *  Metodo para mostrar uma mensagem ao usuário
+     * @param msg
+     */
+    public void mostrarMensagem(String msg){
+        Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 
 }
